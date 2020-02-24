12e0972ee188f095edfd5ae45000188e1a04bd9c
inoublii <inoublii@gmail.com>
2020-02-24 20:49:04 +0100

    done

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 x = ARGV[0].scan(/(from:)(\+?[A-Za-z0-9]+)(...)(to:)(\+?[A-Za-z0-9]+)(...)(flags:)((-?[0|1]:?)+)/)
 print(x[0][1] + "," + x[0][4] + "," + x[0][7])
-
+print("\n")
