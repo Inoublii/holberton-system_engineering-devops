59c9a52501fe47b565ec5ff96492ef6bbcfd2552
inoublii <inoublii@gmail.com>
2020-03-25 21:55:24 +0000

    fx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,28 @@
 # Install ngnix with puppet and configurate a server
-# Requirements:
-#    Nginx should be listening on port 80
-#    When querying Nginx at its root / with a GET request (requesting a page) using curl, it must return a page that contains the string Holberton School
-#    The redirection must be a “301 Moved Permanently”
-
-exec {'update':
+#The redirection must be a “301 Permanently”
+exec {'updates':
   provider => shell,
   path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo apt-get -y update',
+  command  => 'sudo apt-get update -y ',
 }
 
-exec {'install':
+exec {'installation':
   provider => shell,
   path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo apt-get -y install nginx',
+  command  => 'sudo apt-get install nginx -y ',
 }
 
-exec {'html':
+exec {'redirection':
+  provider => shell,
+  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
+}
+
+exec {'html config':
   provider => shell,
   command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
 }
 
-exec {'sedConfig':
-  provider => shell,
-  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
-}
+
 
 exec {'start':
   provider => shell,
