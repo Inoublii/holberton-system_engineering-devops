cf2b575db35d92335049908f1c828ef054ee4f7b
inoublii <inoublii@gmail.com>
2020-05-04 22:10:51 +0100

    fx

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,30 +3,26 @@
 Python script that use API,
 to export data in the JSON format..
 Records all tasks from all employees
-
 '''
+
 import json
 import requests
 
 if __name__ == "__main__":
-    users = requests.get("http://jsonplaceholder.typicode.com/users").json()
-    tasks = requests.get("http://jsonplaceholder.typicode.com/todos").json()
-    storage = {}
-
+    url = requests.get("http://jsonplaceholder.typicode.com/users").json()
+    users = requests.get(url, verify=False).json()
+    username_doc = {}
+    user_doc = {}
     for user in users:
-        em_id = user.get("id")
-        username = user.get("username")
-        all = []
-
-        for task in tasks:
-            if (task.get("userId") == em_id and task.get("completed")):
-                tmp = {}
-                tmp["task"] = task.get("title")
-                tmp["completed"] = task.get("completed")
-                tmp["username"] = username
-                all.append(tmp)
-
-        storage[em_id] = all
-
-    with open("todo_all_employees.json", 'w+') as jsonfile:
-        json.dump(storage, jsonfile)
+        uid = user.get("id")
+        user_doc[uid] = []
+        username_doc[uid] = user.get("username")
+    url = "https://jsonplaceholder.typicode.com/todos"
+    todo = requests.get(url, verify=False).json()
+    [user_doc.get(t.get("userId")).append({"task": t.get("title"),
+                                           "completed": t.get("completed"),
+                                           "username": username_doc.get(
+                                               t.get("userId"))})
+     for t in todo]
+    with open("todo_all_employees.json", 'w') as jsonfile:
+        json.dump(user_doc, jsonfile)
