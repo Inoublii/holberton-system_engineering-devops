01676768a7ff96ac3e29fd35afcc2d0ec50f0731
inoublii <inoublii@gmail.com>
2020-06-11 00:16:41 +0000

    change methode

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-exec { '/etc/default/ngix':
-  command => 'sed -i s/15/4096/g /etc/default/ngix; \
-  /etc/init.d/nginx restart',
-  path    => ['/bin/'],
+exec {'fd':
+path     => ['/usr/bin', '/bin'],
+command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx; sudo service nginx restart",
+provider => 'shell',
 }
